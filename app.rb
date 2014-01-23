8a55427eea71fa735b31ab428d28bc0eb5dbb8d1
Sebastian Geiger <sebastian.geiger@gmail.com>
2014-01-23 22:42:02 +0100

    Logging requests to /entrypoint

--- app.rb
+++ app.rb
@@ -1,5 +1,22 @@
 require 'sinatra'
+LOG_FILE = "log.txt"
 
 get '/hi' do
   "Hello World!"
+end
+
+get '/entrypoint' do
+  File.open(LOG_FILE, "a") do |file|
+    file.write("#{Time.now}: GET request to /entrypoint\n")
+  end
+end
+
+post '/entrypoint' do
+  File.open(LOG_FILE, "a") do |file|
+    file.write("#{Time.now}: POST request to /entrypoint\n")
+  end
+end
+
+get '/log' do
+  IO.read(LOG_FILE)
 end
